c7759aa106966b2fbbda998a947acf8fc18d2881
CherniSamar5 <chernisamar029@gmail.com>
2024-02-18 19:56:45 +0100

    send data to database

--- myApp/app.js
+++ myApp/app.js
@@ -4,21 +4,52 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 app.use(express.urlencoded({ extended: true }));
-const myData = require("./models/mydataSchema")
+const User = require("./models/customerSchema")
 var myRes
 
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
+
+//Get request
 app.get('/', (req, res) => {
   res.render("index", {})
 
 })
 
+app.get('/user/add.html', (req, res) => {
+  res.render("user/add", {})
+
+})
+
+app.get('/user/edit.html', (req, res) => {
+  res.render("user/edit", {})
+
+})
+
+app.get('/user/view.html', (req, res) => {
+  res.render("user/view", {})
+
+})
+
+//Post request 
+app.post('/user/add.html', (req, res) => {
+  //console.log(req.body)
+  const user = new User(req.body)
+  user.save()
+  .then(() => {
+    res.redirect("/user/add.html")
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+})
 
 
 
+//Connect dataBase
 mongoose.connect("mongodb+srv://chernisamar:samar13653110@cluster0.pb5mryo.mongodb.net/all-data?retryWrites=true&w=majority")
   .then(() => {
     app.listen(port, () => {
@@ -29,31 +60,14 @@ mongoose.connect("mongodb+srv://chernisamar:samar13653110@cluster0.pb5mryo.mongo
   .catch((err) => { console.log(err) })
 
 
-
-app.post('/', (req, res) => {
-  //console.log(req.body)
-  //res.redirect("/")
-
-  const mydata = new myData(req.body)
-
-  mydata.save().then(result => {
-    res.redirect("/")
-  }).catch(err => {
-    console.log(err)
-  })
-
-
-})
-
-
 // to reload the page 
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname, 'public'));
 
-
 const connectLivereload = require("connect-livereload");
+const { error } = require('console');
 app.use(connectLivereload());
 
 liveReloadServer.server.once("connection", () => {
